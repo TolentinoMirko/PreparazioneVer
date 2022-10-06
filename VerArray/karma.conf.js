ff43139d4d5cb3bdfac5c02ae186fb3d5078c419
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2022-10-06 10:40:43 +0000

    fatto

--- VerArray/karma.conf.js
+++ VerArray/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tolentino'),
+      dir: require('path').join(__dirname, './coverage/ver-array'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- VerArray/src/app/app.component.spec.ts
+++ VerArray/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Tolentino'`, () => {
+  it(`should have as title 'VerArray'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Tolentino');
+    expect(app.title).toEqual('VerArray');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Tolentino app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('VerArray app is running!');
   });
 });
