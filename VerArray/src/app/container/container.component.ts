64e4964f3aeced3ae2c24f96a2d6d282a8c563c2
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2022-10-07 08:56:59 +0000

    punto 4 non completato

--- VerArray/src/app/container/container.component.ts
+++ VerArray/src/app/container/container.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ContainerComponent implements OnInit {
   names :string[];
   constructor() { 
-    this.names = ['Escavatori','Terne','Pale','Minipale','Dumpers','Bulldozer','Motolivellatrici','Rulli-Compattatori','Piastre-vibranti','Finitrici']
+    this.names = ['Escavatori','Terne','Pale','Minipale','Dumpers','Bulldozer','Motolivellatrici','Rulli-Compattatori','Piastre-vibranti','Finitrici'];
   }
   
   ngOnInit(): void {
