b87310832c2da726339cd3da2da3324bbc12f071
TolentinoMirko <tolentino.mirko@iisgalvanimi.edu.it>
2023-03-09 13:50:55 +0000

    arrivato fino all'esercizio 4 VerRouting

---
+++ VerRouting/src/services/food.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+//per generare un servizio bisogna scrivere ng generate service food
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+}
